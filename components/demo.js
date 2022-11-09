ffb148a550f0405993e406f9c2bc60fa2bdc1e08
Eshaan Bhattad <eshaan2@illinois.edu>
2022-11-09 16:26:35 -0600

    Updated with React Demo

--- components/demo.js
+++ components/demo.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import Button from "react-native-ui-lib/button";
+import { useEffect, useState } from "react";
 
 export default function Demo() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [createProfile, setCreateProfile] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const updateCount = () => {
+    setCount(count + 1);
+    if (count == 10) {
+      setCreateProfile(true);
+    }
+  };
+  if (!createProfile) {
+    return (
+      <View style={styles.container}>
+        <Text>{count}</Text>
+        <Button onPress={updateCount} label="Create Profile"></Button>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>You are creating a profile</Text>
+        <Button onPress={updateCount} label="Create Profile"></Button>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
