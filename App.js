c8d2bd4fd230c317530c482353d35c6dcd04f14b
Mohit Gupta <mohitgupta@Mohits-MacBook-Pro-1621.local>
2022-11-14 15:46:24 -0600

    Introduction Screen for onboarding.

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Demo from "./components/demo";
+import Onboarding from "./components/02.0_onboarding_demo_screen";
+
 export default function App() {
-  return <Demo></Demo>;
+  return <Onboarding></Onboarding>;
 }
 
 const styles = StyleSheet.create({
---
+++ components/02.1_onboarding_screen_data.js
@@ -0,0 +1,20 @@
+export const slideData = [
+    {
+        id: '1',
+        title: 'Quick & Easy Contact Sharing',
+        description : 'Share select or all of your contacts through vfc card on the go.',
+        image: require('../assets/contactly_icons/default.png') ,
+    },
+    {
+        id: '2',
+        title: 'Quick & Easy Contact Sharing',
+        description : 'Share select or all of your contacts through vfc card on the go.',
+        image: require('../assets/contactly_icons/profile.png') ,
+    },
+    {
+        id: '3',
+        title: 'Quick & Easy Contact Sharing',
+        description : 'Share select or all of your contacts through vfc card on the go.',
+        image: require('../assets/contactly_icons/default.png') ,
+    },
+]
