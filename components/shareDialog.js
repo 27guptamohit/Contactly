6bdf1804b30597db62009afd622c5e5ffbd901ed
Eshaan Bhattad <eshaan2@illinois.edu>
2022-12-01 21:16:11 -0600

    Finished QR Code functionltiy

--- components/shareDialog.js
+++ components/shareDialog.js
@@ -1,4 +1,4 @@
-import { Dialog, Button, Colors } from "react-native-ui-lib";
+import { Dialog, Button, Colors, Incubator } from "react-native-ui-lib";
 import QRCode from "react-native-qrcode-svg";
 import Svg, { Path } from "react-native-svg";
 import { Share, StyleSheet, TouchableOpacity } from "react-native";
@@ -6,6 +6,7 @@ import View from "react-native-ui-lib/view";
 import React, { startTransition, useEffect, useState } from "react";
 import vCard from "./vcard/vcard";
 import { getContactCard } from "./vcard/converter";
+
 const onShare = async () => {
   try {
     const result = await Share.share({
@@ -25,33 +26,67 @@ const onShare = async () => {
   }
 };
 
+const addNote = (message, profile) => {
+  profile.notes = message;
+};
+
 export default function ShareContact(props) {
-  return (
-    <Dialog
-      visible={props.visible}
-      panDirection={props.panDirection}
-      onDismiss={props.toggleOff}
-      height={"50%"}
-      width={"75%"}
-    >
-      <View style={styles.container}>
-        <View flex center style={styles.qrcode}>
-          <QRCode size={200} value={getContactCard(props.profile)}></QRCode>
+  const [note, setNote] = useState("");
+  const [isCompleted, setisCompleted] = useState(false);
+
+  if (!isCompleted) {
+    return (
+      <Dialog
+        visible={props.visible}
+        panDirection={props.panDirection}
+        onDismiss={() => {
+              setisCompleted(false);
+              return props.toggleOff;
+        }}
+        height={"50%"}
+        width={"75%"}
+      >
+        <View style={styles.container}>
+          <View flex center style={styles.qrcode}>
+            <Incubator.TextField
+              placeholder={"Add an optional note"}
+              onChangeText={(text) => setNote(text)}
+              showCharCounter
+              width={100}
+              multiline={true}
+            />
+            <Button
+              size={'large'}
+
+              style={styles.button}
+              label={"Continue"}
+              onPress={(pressed) => {if(pressed){ 
+                setisCompleted(true);
+              }}}
+            ></Button>
+          </View>
+          <View flex center></View>
         </View>
-        <View flex center>
-          {/* <Button
-            size={"medium"}
-            style={{ padding: 15 }}
-            borderRadius={10}
-            backgroundColor={"#fff"}
-            iconSource={require("../assets/share.png")}
-            iconStyle={styles.icon}
-            onPress={onShare}
-          /> */}
+      </Dialog>
+    );
+  } else {
+    return (
+      <Dialog
+        visible={props.visible}
+        panDirection={props.panDirection}
+        onDismiss={props.toggleOff}
+        height={"50%"}
+        width={"75%"}
+      >
+        <View style={styles.container}>
+          <View flex center style={styles.qrcode}>
+            <QRCode size={200} value={getContactCard(props.profile, note)}></QRCode>
+          </View>
+          <View flex center></View>
         </View>
-      </View>
-    </Dialog>
-  );
+      </Dialog>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -64,6 +99,8 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
+    height: 500,
+    width: 300,
     borderRadius: 15,
   },
   icon: {
@@ -71,5 +108,8 @@ const styles = StyleSheet.create({
     resizeMode: "contain",
     height: 150,
     width: 150,
+  },
+  button: {
+    marginTop: 100,
   },
 });
--- components/vcard/converter.js
+++ components/vcard/converter.js
@@ -1,11 +1,26 @@
 import vCard from "./vcard";
 import formatter from "./formatter";
-
-export function getContactCard(profile) {
+import { CONTACT_KEYS } from "../../utils/constants";
+export function getContactCard(profile, note) {
   var contactCard = vCard();
-  contactCard.formattedName = profile.title;
-  contactCard.socialUrls = profile.Phone
-  console.log(profile)
-  console.log(contactCard);
+  contactCard.firstName = profile?.firstName;
+  contactCard.lastName = profile?.lastName;
+  contactCard.cellPhone = profile?.cellPhone;
+  contactCard.workPhone = profile?.workPhone;
+  contactCard.email = profile?.email;
+  contactCard.workEmail = profile?.workEmail;
+  contactCard.organization = profile?.organization;
+  contactCard.title = profile?.title;
+  contactCard.birthday = new Date(profile?.birthday);
+  contactCard.url = profile?.url;
+  contactCard.socialUrls[CONTACT_KEYS.facebook] = profile?.facebook;
+  contactCard.socialUrls[CONTACT_KEYS.gitHub] = profile?.gitHub;
+  contactCard.socialUrls[CONTACT_KEYS.instagram] = profile?.instagram;
+  contactCard.socialUrls[CONTACT_KEYS.snapchat] = profile?.snapchat;
+  contactCard.socialUrls[CONTACT_KEYS.tiktok] = profile?.tiktok;
+  contactCard.socialUrls[CONTACT_KEYS.whatsApp] = profile?.whatsApp;
+  contactCard.socialUrls[CONTACT_KEYS.weChat] = profile?.weChat;
+  contactCard.socialUrls[CONTACT_KEYS.linkedIn] = profile?.linkedIn;
+  contactCard.note = note;
   return formatter.getFormattedString(contactCard);
 }
